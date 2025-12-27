390c189342ed0a7356948205b7bd82db7a6ad9e0
Figo Mandala <figomandala@gmail.com>
2025-12-27 22:41:44 +0700

    Add CORS middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
   import express from "express";
+  import cors from "cors";
   import dotenv from "dotenv";
   import authRoutes from "./routes/auth.js";
   import userRoutes from "./routes/user.js";
@@ -9,6 +10,7 @@
   const app = express();
 
   // Middlewares
+  app.use(cors());
   app.use(express.json());
   app.use("/uploads", express.static("uploads"));
 
